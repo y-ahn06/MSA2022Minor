a914396d8878c8beb2786b0701c6f9e71a595a5e
Kristofferson Culmer <krcd58@mail.missouri.edu>
2022-06-16 13:45:54 -0500

    Add files via upload

---
+++ paintJobEstimator.py
@@ -0,0 +1,21 @@
+#INPUT
+#Declare variables for known values
+hourly_labor_cost = 62.25
+unit_of_wall_area = 350
+hours_labor_per_unit = 6
+#prompt the user to enter the amount of wall to paint
+wall_area = input("What is the area of wall in sq/ft: ")
+#prompt user to enter cost of paint per gallon
+paint_price = input("What is the price of paint per gallon: ")
+test_number = wall_area / paint_price
+
+#PROCESS
+#Calculate to hours of labor
+#Calculate the cost of labor
+#Calculate the amount of paint
+#Calculate the cost of the paint
+#Calculate total cost of the job
+
+#OUTPUT
+#Print hours of labor, cost of labor, amount of paint, 
+#cost of paint, total job cost
